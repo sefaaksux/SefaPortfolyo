48bea733022ab71da7c54db83d3a111e1fc0dfcd
sefaaksu <sefaaksusefa@gmail.com>
2024-01-14 20:50:44 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+using SefaPortfolyo.Models;
+
+namespace SefaPortfolyo.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Hakkimda()
+    {
+        return View();
+    }
+    public IActionResult Iletisim()
+    {
+        return View();
+    }
+    
+    public IActionResult Projelerim()
+    {
+        return View();
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SefaPortfolyo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
